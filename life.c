acbb9dd2881d32da7876e11e9d3d7afbe1664c6a
Spyros Gasteratos <morfeas3000@gmail.com>
2013-07-03 14:41:40 +0300

    added input parsing
    Signed-off-by: Spyros Gasteratos <morfeas3000@gmail.com>

--- life.c
+++ life.c
@@ -7,12 +7,23 @@
  * 	argv[1]: Width(N)
  * 	argv[2]: Height(M)
  * 	argv[3]: input filename
+ *
+ *  file has the following structure:
+ *  x-coordinates of alive cell, y-coordinates of alive cell\n
+ * e.g.
+ * 2,5
+ * 3,4
+ * 3,3
+ * 1,2
+ * 150000,23452345
  */
+
 int main(int argc,char **argv){
 
-	char *bfile,**board;
+	char *bfile,**board,*buff,*substr;
 	char d;
 	int i,j,x,y,n,m;
+	FILE *input;
 
 	if( argc <= 3 ){
 		printf("Usage: executable *Board Width* *Board Height* *Input Filename*\n");
@@ -25,14 +36,59 @@ int main(int argc,char **argv){
 
 	//printf("%s\n",bfile);
 
-	*board = malloc( m*sizeof( char* ));
-	if(*board == NULL)
-	exit(-1);
-	for(i = 0; i < m; i++ ){
-		board[i] = malloc(n*sizeof(char));
-		if(board[i] == NULL)
+	*board = malloc(m*sizeof( char* ));
+	if(*board == NULL){
+		printf("Malloc error\n");
 		exit(-1);
 	}
+	for(i = 0; i < m; i++ ){
+		board[i] = malloc(n*sizeof(char));
+		if(board[i] == NULL){
+			printf("Malloc error\n");
+			exit(-1);
+		}
+		memset(board[i], '-', n);
+	}
+	input = fopen(bfile,"r");
+	 if (input == NULL)
+		printf("File doesn't exist\n");
+	buff = malloc(47*sizeof(char));
+	if (buff == NULL){
+		printf("Malloc error\n");
+		goto exit;
+	}
 
+	while (fgets (buff, 47, input) != NULL){
+		substr = strtok(buff,",");
+	//puts(substr);
+		x = atoi(substr);
+		if (x > m )
+			goto exit;
+		substr = strtok(NULL,"\n");
+	//puts(substr);
+		y = atoi(substr);
+		if (y > n )
+			goto exit;
+		board[x][y] = 'A';
+	//printf("%d %d \n", x, y);
+	}
 
+	if( m < 40 && n < 80){
+		printf("Initial Board: \n");
+		for (i = 0; i < m; i++){
+			for (j = 0; j < n; j++){
+				printf("%c ", board[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	/*Play life*/
+exit:
+	for(i = 0; i < m; i++ ){
+		free(board[i]);
+	}
+	free(buff);
+	//free(*board);
+	printf("Out\n");
+return 0;
 }
